8463cf85a721f9b7ee3fdbc189f982a9f430a6bf
AbdulRazaq Suleiman <razaqayomide01@gmail.com>
2025-03-17 22:43:54 +0100

    Implemented JWT

---
+++ src/iam/authentication/authentication.controller.ts
@@ -0,0 +1,37 @@
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { AuthenticationService } from './authentication.service';
+import { SignInDto } from './dto/sign-in.dto';
+import { SignUpDto } from './dto/sign-up.dto';
+
+@Controller('auth')
+export class AuthenticationController {
+  constructor(private readonly authService: AuthenticationService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signUp(@Body() signUpDto: SignUpDto) {
+    return this.authService.signUp(signUpDto);
+  }
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(
+    @Res({ passthrough: true }) response: Response,
+    @Body() signInDto: SignInDto,
+  ) {
+    const accessToken = this.authService.signIn(signInDto);
+    response.cookie('accessToken', accessToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: true,
+    });
+  }
+}
